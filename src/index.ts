cb6c059fd7b2f04d56a22d8814803346d9ab692c
Vincent Pinas <vincentpinas@Vincents-MacBook-Pro.local>
2025-11-04 02:13:39 +0100

    marina.ts contents moved to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import Marina from "./marina.js";
+import * as constants from "./const.js";
+import * as components from "./components/index.js";
+import * as core from "./core/index.js";
+import * as utils from "./utils/index.js";
+
+console.log(`Marina v${constants.VERSION}`);
+
+const Marina = {
+	version: constants.VERSION,
+	...components,
+	...utils,
+	...core
+};
+
+export * from "./components/index.js";
+export * from "./core/index.js";
+export * from "./utils/index.js";
 
 export default Marina;
-
-export * from "./marina.js";
