6fcad7947e79b68db46a7ee016f793b20598b471
Vincent Pinas <vincentpinas@Vincents-MacBook-Pro.local>
2025-11-05 19:07:15 +0100

    version added to default export

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,8 +1,10 @@
+import { VERSION } from "./const";
 import Icon, { setAssetsPath } from "./component/Icon";
 
 customElements.define("m-icon", Icon);
 
 const icons = {
+    version: VERSION,
 	setAssetsPath,
 };
 
